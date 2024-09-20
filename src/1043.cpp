dc16b32b8e4ab0d80a69ae892ec11ebb599d1351
WYQ <1150344015@qq.com>
2024-09-20 20:59:11 +0800

    optimize time complexity of problem 1043

--- src/1043.cpp
+++ src/1043.cpp
@@ -7,10 +7,28 @@ int f[N][K];
 
 int dp(int n, int k) {
     if (f[n][k]) return f[n][k];
-    if (k == 1) return n;
+    if (k == 1) return f[n][k] = n;
+    if (n <= 2) return f[n][k] = n;
     f[n][k] = n;
-    for (int i = 2; i <= n; i++)
-        f[n][k] = min(f[n][k], max(dp(i - 1, k - 1), dp(n - i, k)) + 1);
+    // for (int i = 2; i <= n; i++)
+    //     f[n][k] = min(f[n][k], max(dp(i - 1, k - 1), dp(n - i, k)) + 1);
+    int l = 2, r = n, mid, tmpl, tmpr;
+    while (l < r - 1) {
+        mid = (l + r) >> 1;
+        tmpl = dp(mid - 1, k - 1);
+        tmpr = dp(n - mid, k);
+        if (tmpl == tmpr) {
+            l = r = mid;
+            break;
+        }
+        if (tmpl < tmpr) {
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+    f[n][k] = min(f[n][k], max(dp(l - 1, k - 1), dp(n - l, k)) + 1);
+    f[n][k] = min(f[n][k], max(dp(r - 1, k - 1), dp(n - r, k)) + 1);
     return f[n][k];
 }
 
