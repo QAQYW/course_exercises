5da0e36b74575f280d6ff21a0d65e45e6768f4c3
WYQ <1150344015@qq.com>
2024-10-24 16:54:26 +0800

    1041 没写完

---
+++ src/1041.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ! 没写完
+
+const int N = 1000;
+
+int T;
+int n1, n2, a1[N], a2[N];
+
+int main() {
+    cin >> T;
+    while (T--) {
+        cin >> n1 >> n2;
+        for (int i = 1; i <= n1; i++) cin >> a1[i];
+        for (int i = 1; i <= n2; i++) cin >> a2[i];
+        
+    }
+
+
+    return 0;
+}
