e6be2670fc6cdb99e5666919dc15d22780b94724
WYQ <1150344015@qq.com>
2024-11-04 23:15:09 +0800

    1026 AC

--- src/1026.cpp
+++ src/1026.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// O(n^5), n <= 20
+
 const int N = 25;
 
 int T, n, m;
@@ -8,16 +10,12 @@ long long a[N], sum[N], f[N][N][N];
 // m 个乘号
 
 int main() {
-    FILE *fp = fopen("src\\input", "r");
     sum[0] = 0;
-    // scanf("%d", &T);
-    fscanf(fp, "%d", &T);
+    scanf("%d", &T);
     while (T--) {
-        // scanf("%d%d", &n, &m);
-        fscanf(fp, "%d%d", &n, &m);
+        scanf("%d%d", &n, &m);
         for (int i = 1; i <= n; i++) {
-            // scanf("%lld", &a[i]);
-            fscanf(fp, "%lld", &a[i]);
+            scanf("%lld", &a[i]);
             sum[i] = sum[i - 1] + a[i];
         }
         memset(f, 0x00, sizeof(f));
@@ -26,21 +24,26 @@ int main() {
                 f[l][r][0] = sum[r] - sum[l - 1];
             }
         }
-        for (int len = 2)
-
-        // f[1][1] = a[1];
-        // for (int i = 2; i <= n; i++) {
-        //     f[i][0] = sum[i];
-        //     int jMax = min(i - 1, m);
-        //     for (int j = 1; j <= jMax; j++) {
-        //         int kMin = max(1, j);
-        //         for (int k = i - 1; k; k--) {
-        //             f[i][j] = max(f[i][j], f[k][j - 1] * (sum[i] - sum[k]));
-        //         }
-        //     }
-        // }
-        // printf("%lld\n", f[n][m]);
+        for (int len = 2; len <= n; len++) {
+            for (int l = 1, r = len; r <= n; l++, r++) {
+                // k = 0
+                f[l][r][0] = sum[r] - sum[l - 1];
+                // k >= 1
+                for (int k = 1; k <= r - l && k <= m; k++) {
+                    for (int mid = l; mid < r; mid++) {
+                        for (int i = 0; i < k && i <= mid - l; i++) {
+                            if (k - i <= r - mid) { // if (k - 1 - i <= r - mid - 1)
+                                f[l][r][k] = max(f[l][r][k], f[l][mid][i] * f[mid + 1][r][k - 1 - i]);
+                            }
+                            if (k - i <= r - mid - 1) {
+                                f[l][r][k] = max(f[l][r][k], f[l][mid][i] + f[mid + 1][r][k - i]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        printf("%lld\n", f[1][n][m]);
     }
-    fclose(fp);
     return 0;
 }
